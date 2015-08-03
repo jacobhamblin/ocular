671c7a6ce3fd7675844779aaa8327b2531cc0e31
lawlerblader <jacob@hamblin.cc>
2015-08-02 19:14:37 -0700

    basic upvoting

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -5,3 +5,10 @@ Template.postItem.helpers({
     return a.hostname;
   }
 });
+
+Template.postItem.events({
+  'click .upvote': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
+});
